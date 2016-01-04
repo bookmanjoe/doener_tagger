9dc76315ed4cb9eb2c47e8e299edc7a55cf5cad8
bookmanjoe <bookmanjens@googlemail.com>
2016-01-04 19:34:01 +0100

    Initial commit

---
+++ app/src/test/java/com/gruppe10/mobilecommerce/doener_tagger/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.gruppe10.mobilecommerce.doener_tagger;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
